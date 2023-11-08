b6f35727ff55ca8c686cf939126485aff4f7af57
Tobias Ledin <tobias.ledin@gmail.com>
2023-11-08 01:06:32 +0100

    Förenklad prop names, lagt till prop för password för kundinloggning.

--- Customer.cs
+++ Customer.cs
@@ -9,16 +9,24 @@ namespace Paruppgift_e_handel
     internal class Customer
     {
         public int CustomerId { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerAddress { get; set; }
-        public string CustomerPhone { get; set; }
-        public string CustomerEmail { get; set; }
-        public virtual List<CustomerOrder> CustomerOrders { get; set; } = new List<CustomerOrder>();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public virtual List<CustomerOrder> CustomerOrders { get; set; }
         //bonus/kundklass-prop
 
         public override string? ToString()
         {
-            return $"Customer ID: [{CustomerId}] Name: {CustomerName}";
+            return $"[{CustomerId}] Firstname: {FirstName} Lastname: {LastName}";
         }
 
     }
