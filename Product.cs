fe6c4eb8b8e6d013e0a4b7d6627c10eb498f5c18
Tobias Ledin <tobias.ledin@gmail.com>
2023-11-09 01:51:17 +0100

    Fix

--- Product.cs
+++ Product.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Paruppgift_e_handel
 {
+    public enum Category
+    {
+        Fruit =1,
+        Vegetable,
+        Dairy,
+        Meat,
+        Fish,
+        Poultry
+    }
+
     internal class Product
     {
-        public Product()
-        {
-        
-        }
         public int ProductId { get; set; }
-        public string ProductDescription { get; set; }
-        public string ProductBrand { get; set; }
-        public string ProductCategory { get; set; }
-        public double ProductPrice { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        [Column(TypeName = "nvarchar(24)")]     //Force enum to string conversion.
+        public Category Category { get; set; }
+        public double Price { get; set; }
 
         public override string? ToString()
         {
-            return $"{ProductDescription}, {ProductPrice}:-";
+            return $"Description: {Description}\t\tBrand: {Brand}\t\tCategory: {Category}\t\tPrice: {Price}";
         }
 
         //public string ProductAmountPerPack { get; set; }
